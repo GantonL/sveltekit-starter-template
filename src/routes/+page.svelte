06baedbece1474538da580ec5f2784b7163a6c0f
GantonL <45093712+GantonL@users.noreply.github.com>
2025-03-25 18:35:26 +0200

    wip-temp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,74 @@
 <script lang="ts">
 	import { page } from "$app/state";
-
-  const response = $state(page.data.data);
   
+  const response = $state(page.data);
+  let count = $state(response.count);
+  let next = $state(response.next);
+  let prev = $state(response.prev);
+  let results = $state(response.results);
+  let searchTerm = '';
+
+  function onSearch(event: Event) {
+    searchTerm = event?.target?.value ?? '';
+    getResults();
+  }
+
+  function onPrev() {
+    getResults(prev ?? 1);
+  }
+  
+  function onNext() {
+    getResults(next ?? 1);
+  }
+
+  function getResults(page?: number) {
+    console.log(page);
+    fetch(`/?search=${searchTerm}&page=${page ?? ''}`)
+    .then(res => res.json().then(res => {
+      count = res.count;
+      next = res.next;
+      prev = res.prev;
+      results = res.results;
+    }));
+  }
+
 </script>
-<div class="flex flex-col">
-  <h3>Data from API:</h3>
-  {#each response as item}
-    <div>id: {item.id} name: {item.name}</div>
-  {/each}
+<div class="flex flex-col items-center w-full p-8">
+  <div class="flex flex-col items-center min-w-2xl gap-4">
+    <header class="flex flex-row items-center justify-between w-full">
+      <h3 class="font-bold text-xl">Start Wars Explorer</h3>
+      <input
+        onchange={onSearch} 
+        class="h-6 border rounded-md" 
+        placeholder="search..."/>
+    </header>
+    <main class="flex flex-col gap-4 w-full">
+      {#each results as item}
+        <div class="flex flex-row items-center justify-between p-4 border rounded-md w-full">
+          <span class="flex-">{item.name}</span>
+          <div class="flex flex-row items-center gap-4">
+            <span>{item.gender}</span>
+            <span>{item.homeworld}</span>
+          </div>
+        </div>
+      {/each}
+    </main>
+    <footer class="flex flex-row items-center justify-between w-full">
+      <span>Total results: {count}</span>;
+      <div class="flex flex-row items-center gap-4">
+        <button
+        class="border rounded-md p-2"
+        class:cursor-pointer={!!prev}
+        onclick={onPrev}
+        disabled={!prev}
+        >Prev</button>
+        <button
+        class="border rounded-md p-2"
+        class:cursor-pointer={!!next}
+        onclick={onNext}
+        disabled={!next}
+        >Next</button>
+      </div>
+    </footer>
+  </div>
 </div>
--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,8 +1,41 @@
-import { API_URL } from "$env/static/private";
-import { json } from "@sveltejs/kit";
+import { json, normalizeUrl, type RequestEvent } from "@sveltejs/kit";
+const homeWorldCache: Record<number, string> = {};
 
-export const GET = async () => {
-  const response = await fetch(`${API_URL}/data`);
+export const GET = async (request: RequestEvent) => {
+  const searchTerm = request.url.searchParams.get('search') ?? '';
+  const page = request.url.searchParams.get('page') ?? '';
+  const response = await fetch(`https://swapi.dev/api/people?search=${searchTerm}&page=${page}`);
   const data = await response.json();
-  return json({data});
+  const count = data.count;
+  const next = normalizeUrl(data.next).url.searchParams.get('page');
+  const prev = normalizeUrl(data.previous).url.searchParams.get('page');
+  const results = data.results.map(res => {
+    return {
+      name: res.name,
+      gender: res.gender,
+      homeworld: res.homeworld
+    }
+  });
+  const homeworldNamesPromises = await Promise.allSettled(results.map(r => getHomeworldName(r.homeworld)));
+  homeworldNamesPromises.forEach((res, index) => {
+    if (res.status === 'fulfilled') {
+      results[index].homeworld = res.value;
+      // TODO parse homeworld link to get index and set each to cache
+      // then get from cache when retreieving data
+    } else {
+      results[index].homeworld = 'n/a';
+    }
+  });
+  return json({
+    count,
+    next,
+    prev, 
+    results,
+  });
+}
+
+const getHomeworldName = async (homeworldLink: string) => {
+  const response = await fetch(homeworldLink);
+  const data = await response.json();
+  return data?.name ?? 'n/a';
 }
